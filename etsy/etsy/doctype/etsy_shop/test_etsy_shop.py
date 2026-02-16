f253da74f40c5ac2b91fdf48c9b75c2aeb02bba0
Christopher Maeurer <260913780+maeurerdev@users.noreply.github.com>
2026-02-16 10:13:07 +0100

    Setup CI pipeline and add tests (#1)
    * Add basic test for Etsy Shop
    
    * Fix CI: set Python to 3.11 and Node to 20 for Frappe v15
    
    * Fix CI: add install helper script with full ERPNext setup
    
    * Fix tests: add before_tests hook

--- etsy/etsy/doctype/etsy_shop/test_etsy_shop.py
+++ etsy/etsy/doctype/etsy_shop/test_etsy_shop.py
@@ -1,9 +1,43 @@
-# Copyright (c) 2026, Cornstarch3D and Contributors
-# See license.txt
-
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
+from etsy.etsy.doctype.etsy_shop.etsy_shop import (
+	short_title,
+)
 
-class TestEtsyShop(FrappeTestCase):
-	pass
+
+class TestShortTitle(FrappeTestCase):
+	"""Tests for the short_title utility function."""
+
+	def test_simple_title(self):
+		self.assertEqual(short_title("Simple Title"), "Simple Title")
+
+	def test_comma_split(self):
+		self.assertEqual(short_title("Part One, Part Two, Part Three"), "Part One")
+
+	def test_semicolon_split(self):
+		self.assertEqual(short_title("Part One; Part Two"), "Part One")
+
+	def test_pipe_split(self):
+		self.assertEqual(short_title("Part One | Part Two"), "Part One")
+
+	def test_bullet_split(self):
+		result = short_title("Part One \u2022 Part Two")
+		self.assertEqual(result, "Part One")
+
+	def test_dash_split(self):
+		self.assertEqual(short_title("Part One - Part Two"), "Part One")
+
+	def test_endash_split(self):
+		result = short_title("Part One \u2013 Part Two")
+		self.assertEqual(result, "Part One")
+
+	def test_html_entity_replacement(self):
+		self.assertEqual(short_title('He said &quot;hello&quot;'), 'He said "hello"')
+
+	def test_truncation_to_60_chars(self):
+		long_title = "A" * 100
+		self.assertEqual(len(short_title(long_title)), 60)
+
+	def test_strips_whitespace(self):
+		self.assertEqual(short_title("  Hello World  , more stuff"), "Hello World")
--- etsy/install.py
+++ etsy/install.py
@@ -1,5 +1,7 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.utils import now_datetime
+from erpnext.setup.utils import enable_all_roles_and_domains
 
 
 def after_install():
@@ -41,3 +43,60 @@ def delete_custom_fields(doctype, fields):
 		},
 	)
 	frappe.clear_cache(doctype=doctype)
+
+
+### setup for testing
+def before_tests():
+	frappe.clear_cache()
+	# complete setup if missing
+	from frappe.desk.page.setup_wizard.setup_wizard import setup_complete
+
+	year = now_datetime().year
+	if not frappe.get_list("Company"):
+		setup_complete(
+			{
+				"currency": "INR",
+				"full_name": "Test User",
+				"company_name": "Wind Power LLC",
+				"timezone": "Asia/Kolkata",
+				"company_abbr": "WP",
+				"industry": "Manufacturing",
+				"country": "India",
+				"fy_start_date": f"{year}-01-01",
+				"fy_end_date": f"{year}-12-31",
+				"language": "english",
+				"company_tagline": "Testing",
+				"email": "test@erpnext.com",
+				"password": "test",
+				"chart_of_accounts": "Standard",
+				"domains": ["Manufacturing"],
+			}
+		)
+
+	frappe.db.set_value("Stock Settings", None, "auto_insert_price_list_rate_if_missing", 0)
+	enable_all_roles_and_domains()
+	create_tax_account()
+
+def create_tax_account():
+	company = "Wind Power LLC"
+	account_name = "Output Tax GST"
+
+	parent = (
+		frappe.db.get_value("Account", {"company": company, "account_type": "Tax", "is_group": 1})
+		or "Duties and Taxes - WP"
+	)
+
+	frappe.get_doc(
+		{
+			"doctype": "Account",
+			"account_name": account_name,
+			"is_group": 0,
+			"company": company,
+			"root_type": "Liability",
+			"report_type": "Balance Sheet",
+			"account_currency": "INR",
+			"parent_account": parent,
+			"account_type": "Tax",
+			"tax_rate": 18,
+		}
+	).insert()
